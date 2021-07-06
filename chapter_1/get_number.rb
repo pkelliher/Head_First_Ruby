4c466c3ac20cb90c04610bb0ff5f4a1019d54180
pkelliher <pkelliher74@gmail.com>
2021-07-05 18:11:23 -0700

    create random num and store in target

--- chapter_1/get_number.rb
+++ chapter_1/get_number.rb
@@ -9,3 +9,8 @@ input = gets
 name = input.chomp
 
 puts "Welcome, #{name}!"
+
+# Store a random number for the player to guess.
+puts "I've got a random number between 1 and 100."
+puts "can you guess it?"
+target = rand(100) + 1
