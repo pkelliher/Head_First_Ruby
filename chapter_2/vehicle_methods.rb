50d4af35094cc153b1f7be6690aa58d7c75752d8
pkelliher <pkelliher74@gmail.com>
2021-07-07 18:43:10 -0700

    default use_headlights method  param)

--- chapter_2/vehicle_methods.rb
+++ chapter_2/vehicle_methods.rb
@@ -8,7 +8,7 @@ def sound_horn
     puts "Beep beep!"
 end
 
-def use_headlights(brightness)
+def use_headlights(brightness = "low-beam")
     puts "Turning on #{brightness} headlights"
     puts "Watch out for the deer!"
 end
@@ -16,3 +16,4 @@ end
 sound_horn
 accelerate
 use_headlights("high-beam")
+use_headlights
