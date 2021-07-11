8389795970ef39b8b9611c95d4ba5ef9f46bac0e
pkelliher <pkelliher74@gmail.com>
2021-07-11 16:44:00 -0700

    Creation of Animal script

---
+++ chapter_2/animal.rb
@@ -0,0 +1,32 @@
+class Animal
+    
+    attr_reader :name, :age
+
+    def name= (value)
+        if value == ""
+            raise "Name can't be blank!"
+        else
+            @name = value
+        end
+    end
+
+    def age= (value)
+        if value < 0
+            raise "An age of #{value} isn't valid!"
+        else
+            @age = value
+        end
+    end
+
+    def move(destination)
+        puts "#{@name} runs to the #{destination}."
+    end
+
+    def talk
+        puts "#{@name} says Bark!"
+    end
+
+    def report_age
+        puts "#{@name} is #{@age} years old."
+    end
+end
