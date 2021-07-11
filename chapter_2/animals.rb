9583c976c35bab2bc873bb806b934c845cb30067
pkelliher <pkelliher74@gmail.com>
2021-07-10 17:31:15 -0700

    completed dog class

--- chapter_2/animals.rb
+++ chapter_2/animals.rb
@@ -1,56 +1,40 @@
-class Bird
-    # talk bird method
-    def talk (name)
-        puts "#{name} says Chirp! Chirp!"
-    end
-    def move (name, destination)
-        puts "#{name} flies to the #{destination}."
-    end
-end
-
 class Dog
     
-    attr_accessor :name, :age
+    attr_reader :name, :age
 
-    def report_age
-        puts "#{@name} is #{@age} years old."
+    def name= (value)
+        if value == ""
+            raise "Name can't be blank!"
+        else
+            @name = value
+        end
     end
 
-    def talk
-        puts "#{@name} says Bark!"
+    def age= (value)
+        if value < 0
+            raise "An age of #{value} isn't valid!"
+        else
+            @age = value
+        end
     end
 
     def move(destination)
         puts "#{@name} runs to the #{destination}."
     end
 
-end
-
-class Cat
-    def talk (name)
-        puts "#{name} says Meow!"
+    def talk
+        puts "#{@name} says Bark!"
     end
-    def move(name, destination)
-        puts "#{name} runs to the #{destination}."
+
+    def report_age
+        puts "#{@name} is #{@age} years old."
     end
 end
 
-fido = Dog.new
-fido.name = "Fido"
-fido.age = 2
-rex = Dog.new
-rex.name = "Rex"
-rex.age = 3
-fido.report_age
-rex.report_age
 
-
-# cat = Cat.new
-# cat_name = "Fluffy"
-# cat.talk(cat_name)
-# cat.move(cat_name, "litter box")
-
-# bird = Bird.new
-# bird_name = "Tweety"
-# bird.talk(bird_name)
-# bird.move(bird_name, "tree")
+dog = Dog.new
+dog.name = "Ted"
+dog.age = 1
+dog.report_age
+dog.talk
+dog.move("bed")
