36e66ed568ea81a499430c57d5aa7f3bbe5e1b23
pkelliher <pkelliher74@gmail.com>
2021-07-18 17:51:59 -0700

    Refund prices amounts

--- chapter_5/arrays.rb
+++ chapter_5/arrays.rb
@@ -19,3 +19,15 @@ end
 prices = [3.99, 25.00, 8.99]
 
 puts format("%.2f", total(prices))
+
+def refund(prices)
+    amount = 0
+    index = 0
+    while index < prices.length
+        amount -= prices[index]
+        index += 1
+    end
+    amount
+end
+
+puts format("%.2f", refund(prices))
