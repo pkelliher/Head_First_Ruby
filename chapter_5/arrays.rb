22066f0cdf6c2b5584d728f57ee2696e2e6956de
pkelliher <pkelliher74@gmail.com>
2021-07-21 19:51:37 -0700

    refactored show_discounts method with code block

--- chapter_5/arrays.rb
+++ chapter_5/arrays.rb
@@ -64,13 +64,24 @@ prices = [3.99, 25.00, 8.99]
 puts format("%.2f", refund(prices))
 
 #show discounts method
+# def show_discounts(prices)
+#     index = 0
+#     while index < prices.length
+#         amount_off = prices[index] / 3.0
+#         puts format("Your discount: $%.2f", amount_off)
+#         index += 1
+#     end
+# end
+
+# show_discounts(prices)
+
+# Refactored show_discounts method with code block
 def show_discounts(prices)
-    index = 0
-    while index < prices.length
-        amount_off = prices[index] / 3.0
+    prices.each do |price|
+        amount_off = price / 3.0
         puts format("Your discount: $%.2f", amount_off)
-        index += 1
     end
 end
 
+prices = [3.99, 25.00, 8.99]
 show_discounts(prices)
