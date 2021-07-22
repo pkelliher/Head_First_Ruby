a2260db2515790aefbfbeee13d8a598cf121bfee
pkelliher <pkelliher74@gmail.com>
2021-07-21 19:39:28 -0700

    refactored refund with code block

--- chapter_5/arrays.rb
+++ chapter_5/arrays.rb
@@ -38,15 +38,28 @@ prices = [3.99, 25.00, 8.99]
 puts format("%.2f", total(prices))
 
 #refund method
+# def refund(prices)
+#     amount = 0
+#     index = 0
+#     while index < prices.length
+#         amount -= prices[index]
+#         index += 1
+#     end
+#     amount
+# end
+
+# puts format("%.2f", refund(prices))
+
+# Refactored Refund method
 def refund(prices)
     amount = 0
-    index = 0
-    while index < prices.length
-        amount -= prices[index]
-        index += 1
+    prices.each do |price|
+        amount -= price
     end
     amount
 end
+
+prices = [3.99, 25.00, 8.99]
 
 puts format("%.2f", refund(prices))
 
