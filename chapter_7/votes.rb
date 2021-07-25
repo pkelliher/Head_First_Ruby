e4fe1c5a9f148287635ecef02ed7493b2b74f764
pkelliher <pkelliher74@gmail.com>
2021-07-24 17:56:23 -0700

    conditional plus looping code block and votes has creation

--- chapter_7/votes.rb
+++ chapter_7/votes.rb
@@ -1,6 +1,17 @@
 lines = []
 File.open("votes.txt") do |file|
-    lines = file.readlines
+  lines = file.readlines
+end
+
+votes = {}
+
+lines.each do |line|
+  name = line.chomp
+  if votes[name] != nil
+    votes[name] += 1
+  else
+    votes[name] = 1
+  end
 end
 
 p lines
