7e1f9cd8b4d0b19f88c920bd07e78fdf565bfa26
pkelliher <pkelliher74@gmail.com>
2021-07-25 17:26:08 -0700

    Removed conditional if/else, because we included Hash.new with a default object of '0'

--- chapter_7/votes.rb
+++ chapter_7/votes.rb
@@ -3,15 +3,11 @@ File.open("votes.txt") do |file|
   lines = file.readlines
 end
 
-votes = {}
+votes = Hash.new(0)
 
 lines.each do |line|
   name = line.chomp
-  if votes[name]
-    votes[name] += 1
-  else
-    votes[name] = 1
-  end
+  votes[name] += 1
 end
 
 p votes
