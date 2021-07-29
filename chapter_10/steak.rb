0a5824d8810ef485fbb5b679347a4cfc2676b5c4
pkelliher <pkelliher74@gmail.com>
2021-07-28 19:09:17 -0700

    head scratcher

--- chapter_10/steak.rb
+++ chapter_10/steak.rb
@@ -18,10 +18,17 @@ class Steak
 
 end
 
-first_steak = Steak.new
-first_steak.grade = "Prime"
-second_steak = Steak.new
-second_steak.grade = "Choice"
+prime = Steak.new
+prime.grade = "Prime"
+choice = Steak.new
+choice.grade = "Choice"
+select = Steak.new
+select.grade = "Select"
 
-puts first_steak <=> second_steak
-puts second_steak <=> first_steak
+puts "prime > choice: #{prime > choice}"
+puts "prime < select: #{prime < select}"
+puts "select == select: #{select == select}"
+puts "select <= select: #{select <= select}"
+puts "select >= choice: #{select >= choice}"
+print "choice.between?(select, prime): "
+puts choice.between?(select, prime)
