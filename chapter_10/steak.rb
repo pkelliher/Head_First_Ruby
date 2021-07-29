93d019eb0b933e71c0509a85f6aa339a0869bf50
pkelliher <pkelliher74@gmail.com>
2021-07-28 18:58:22 -0700

    included logic for spaceshipt operator

--- chapter_10/steak.rb
+++ chapter_10/steak.rb
@@ -1,13 +1,17 @@
 class Steak
+  GRADE_SCORES = { "Prime" => 3, "Choice" => 2, "Select" => 1 }
 
-    GRADE_SCORES = {"Prime" => 3, "Choice" => 2, "Select" => 1}
+  attr_accessor :grade
 
-    attr_accessor :grade
-
-    def >(other)
-        GRADE_SCORES[grade] > GRADE_SCORES[other.grade]
+  def <=>(other)
+    if GRADE_SCORES[self.grade] < GRADE_SCORES[other.grade]
+      return -1
+    elsif GRADE_SCORES[self.grade] == GRADE_SCORES[other.grade]
+      return 0
+    else
+      reuturn 1
     end
-
+  end
 end
 
 first_steak = Steak.new
@@ -15,6 +19,5 @@ first_steak.grade = "Prime"
 second_steak = Steak.new
 second_steak.grade = "Choice"
 
-if first_steak > second_steak
-    puts "I'll take the #{first_steak.inspect}."
-end
+puts first_steak <=> second_steak
+puts second_steak <=> first_steak
