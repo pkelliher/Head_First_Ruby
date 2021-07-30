61fb66a37e4c88d197bd8e58eb658f55e2fea042
pkelliher <pkelliher74@gmail.com>
2021-07-29 19:41:17 -0700

    Adding comments to the code that will then display in the rdoc index page after performing rdoc on my file within the terminal

--- chapter_11/word_splitter1.rb
+++ chapter_11/word_splitter1.rb
@@ -1,9 +1,14 @@
+# This class allows you to perform various operations
+# on the words in a string.
 class WordSplitter1
 
     include Enumerable
-
+    
+    # The string to split into words.
     attr_accessor :string
-
+    
+    # Passes each word in the string to a block, one
+    # at a time.
     def each
         string.split(" ").each do |word|
             yield word
