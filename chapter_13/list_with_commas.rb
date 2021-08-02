176b792909493ae1adf442b2b457329c5855dd95
pkelliher <pkelliher74@gmail.com>
2021-08-01 17:49:49 -0700

    ListWithCommas class, with a join method that adds and before the last item in the items array.

--- chapter_13/list_with_commas.rb
+++ chapter_13/list_with_commas.rb
@@ -6,3 +6,10 @@ class ListWithCommas
         "#{other_items} #{last_item}"
     end
 end
+
+two_subjects = ListWithCommas.new
+two_subjects.items = ['my_parents', 'a rodeo clown']
+puts "A photo of #{two_subjects.join}"
+three_subjects = ListWithCommas.new
+three_subjects.items = ['my parents', 'a rodeo clown', 'a prize bull']
+puts "A photo of #{three_subjects.join}"
