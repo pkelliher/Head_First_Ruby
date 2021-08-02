35c37555fcf39af7c58479d332f9c672ef5202b3
pkelliher <pkelliher74@gmail.com>
2021-08-01 18:15:28 -0700

    included an additional comment between other_items and last_items

--- chapter_13/list_with_commas.rb
+++ chapter_13/list_with_commas.rb
@@ -3,7 +3,7 @@ class ListWithCommas
     def join
         last_item = "and #{items.last}"
         other_items = items.slice(0, items.length - 1).join(', ')
-        "#{other_items} #{last_item}"
+        "#{other_items}, #{last_item}"
     end
 end
 
