cc61b1046859eac84081f2542228a83fc59431c4
pkelliher <pkelliher74@gmail.com>
2021-08-03 18:52:06 -0700

    using assert_equal instead of assert

--- chapter_13/commas_project/test/test_list_with_commas.rb
+++ chapter_13/commas_project/test/test_list_with_commas.rb
@@ -6,19 +6,19 @@ class TestListWithCommas < Minitest::Test
     def test_it_prints_one_word_alone
         list = ListWithCommas.new
         list.items = ['apple']
-        assert('apple' == list.join, "Return value didn't equal 'apple'")
+        assert_equal('apple', list.join)
     end
 
     def test_it_joins_two_words_with_and
         list = ListWithCommas.new
         list.items = ['apple', 'orange']
-        assert('apple and orage' == list.join)
+        assert_equal('apple and orage', list.join)
     end
 
     def test_it_joins_three_words_with_commas
         list = ListWithCommas.new
         list.items = ['apple', 'orange', 'pear']
-        assert('apple, orange, and pear' == list.join)
+        assert_equal('apple, orange, and pear', list.join)
     end
 
 end
