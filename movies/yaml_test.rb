98a861b5afaf78809516d20cb0f3f6626236b893
pkelliher <pkelliher74@gmail.com>
2021-08-05 19:57:29 -0700

    created test.yml by running the yaml_test.rb

--- movies/yaml_test.rb
+++ movies/yaml_test.rb
@@ -5,7 +5,7 @@ store = YAML::Store.new('test.yml')
 
 first_movie = Movie.new
 first_movie.title = "Spirited Away"
-first_movie.direcctor = "Hayao Miyazaki"
+first_movie.director = "Hayao Miyazaki"
 first_movie.year = 2001
 
 second_movie = Movie.new
